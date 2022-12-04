a07e77e648a51d59759baecc8e7c50f2e9c5a5d4
Lavanya Mishra <88896522+Lava-Iris@users.noreply.github.com>
2022-12-04 18:15:19 +0800

    Create Day 4 part 1.py

---
+++ Day 4 part 1.py
@@ -0,0 +1,16 @@
+f = open("input 4.txt", "r")
+input = f.read().split('\n')
+
+sum = 0
+
+for pair in input:
+  pair = pair.split(',')
+  section1 = pair[0].split('-')
+  section2 = pair[1].split('-')
+  
+  min = section2 if int(section2[0]) < int(section1[0]) else section1
+  max = section2 if min == section1 else section1
+  
+  sum += int(min[0] == max[0] or int(min[1]) >= int(max[1]))
+      
+print(sum)
