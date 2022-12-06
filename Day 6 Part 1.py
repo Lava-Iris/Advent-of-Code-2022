d08d61e6e1e0977bb31709aa61e7e236aff77c73
Lavanya Mishra <88896522+Lava-Iris@users.noreply.github.com>
2022-12-06 13:39:26 +0800

    Create Day 6 Part 1.py

---
+++ Day 6 Part 1.py
@@ -0,0 +1,18 @@
+f = open("input 6.txt", "r")
+input = f.read()
+
+n = 4
+
+prev_3 = [0] * (n - 1)
+pointer = 0
+
+for i in range(n - 1):
+  prev_3[i] = input[i]
+  
+for i in range(n - 1, len(input)):
+  if input[i] not in prev_3 and len(prev_3) == len(set(prev_3)):
+    print(i + 1)
+    break
+  else:
+    prev_3[pointer] = input[i]
+    pointer = 0 if pointer == n - 2 else pointer + 1
